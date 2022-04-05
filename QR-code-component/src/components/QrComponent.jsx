8a4d7229f74f812aa27901188a841f9f4e503ff0
mmreza <mohammadrezasafari23@gmail.com>
2022-04-05 15:19:33 +0430

    compelete component

--- QR-code-component/src/components/QrComponent.jsx
+++ QR-code-component/src/components/QrComponent.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import classes from "./styles.module.css"
+import "../assets/styles/style.css";
 
-function QrComponent({ src }) {
+const QrComponent = ({ src }) => {
+  console.log(src);
   return (
-    <div className={classes.card}>
-      <img src={src} className="qr-img" id={`img-${src}`} alt="QR-IMAGE" />
-      <div className="title">
-        <h2>Improve your front-end skills by building projects</h2>
-      </div>
+    <div className="card">
+      <img src={src} className="qr-image" id={`img-${src}`} alt="QR-img" />
       <div className="description">
-          <p>scan the QR code to visit frontend mentor and take your coding skills to the next level</p>
+        <h2 id="h-title">Improve your front-end skills by building projects</h2>
+        <p id="p-description">
+          scan the QR code to visit frontend mentor and take your coding skills
+          to the next level
+        </p>
       </div>
     </div>
   );
-}
+};
 
-
-export default QrComponent ;
+export default QrComponent;
